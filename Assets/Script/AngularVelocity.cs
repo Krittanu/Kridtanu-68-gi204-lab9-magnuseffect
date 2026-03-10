7967912c7252ee6a926b26b69ae72425e2184a6b
Krittanu <Fortunejustaway@gmail.com>
2026-03-10 10:03:09 +0700

    Rotate with AngularVelocity

---
+++ Assets/Script/AngularVelocity.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class AngularVelocity : MonoBehaviour
+{
+    public float angularspeed;
+    Rigidbody rb;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>(); 
+    }
+
+    
+    private void FixedUpdate()
+    {
+        //Press 'A' to ratate using Angular Velocity
+        if (Keyboard.current.aKey.isPressed)
+        {
+            rb.angularVelocity = new Vector3(0, angularspeed, 0);
+        }
+            
+           
+    }
+}
+
+
+
+
